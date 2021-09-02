f99a4a9e095ad8fafbb83f560b185a5f25c1adf2
Edgar Mancia <eddiemm@users.noreply.github.com>
2021-09-02 15:31:50 -0700

    Add menu slide in

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 console.log('Jotter canvas ready');
+
+// DOM variables
+const menu = document.querySelector('.js-menu');
+const hamburger = document.querySelector('.js-hamburger-wrapper');
+const hamburgerIcon = document.querySelector('.js-hamburger-icon');
+
+// change menu visibility
+function toggleMenu (){
+    menu.classList.toggle('menu--is-visible');
+    hamburgerIcon.classList.toggle('hamburger--minus');
+}
+
+// attach click listener to menu icon
+hamburger.addEventListener('click', toggleMenu, false);
+
+
