13435ca1246ea68b3385d0c50315dfda7d888bb6
Edgar Mancia <eddiemm@users.noreply.github.com>
2021-09-02 18:36:57 -0700

    Add editing tool button behavior

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 console.log('Jotter canvas ready');
 
+/* MENU */
 // DOM variables
 const menu = document.querySelector('.js-menu');
 const hamburger = document.querySelector('.js-hamburger-wrapper');
@@ -15,3 +16,21 @@ function toggleMenu (){
 hamburger.addEventListener('click', toggleMenu, false);
 
 
+/* TOOLBAR */
+const toolbar = document.querySelector('.js-toolbar');
+const editingTools = toolbar.children;
+
+// add click listener to parent for event delegation
+toolbar.addEventListener('click', (event) => {
+    let toolElement = event.target.tagName;
+
+    // only toggle the selected class if the element is the parent div or the
+    // the icon within it
+    if(toolElement === 'LI'){
+        event.target.classList.toggle('toolbar__tool--is-selected')
+    }
+    if(toolElement === 'I'){
+        event.target.parentElement.classList.toggle('toolbar__tool--is-selected')
+    }
+
+}, false);
