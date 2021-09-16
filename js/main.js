d47c3b6ddfd946cc461ac91f398d927d5f32bfda
Edgar Mancia <eddiemm@users.noreply.github.com>
2021-09-16 16:31:19 -0700

    Implement search bar to find terms in each note

--- js/main.js
+++ js/main.js
@@ -33,17 +33,20 @@ for(let i = 0; i < toolbar.children.length; i ++){
 // add click listener to parent for event delegation
 toolbar.addEventListener('click', (evt) => {   
     let toolElement = evt.target;
-    // only toggle the selected class if the element is the parent li or the
-    // the icon within it
-    if(toolElement.tagName === 'BUTTON'){        
-        toolElement.classList.toggle('toolbar__tool--is-selected');
-        toggleToolById(toolElement.id);   
-    }   
 
-    if(toolElement.tagName === 'I'){
-        toolElement.parentElement.classList.toggle('toolbar__tool--is-selected')
-        toggleToolById(toolElement.parentElement.id);  
-    }
+    if(document.activeElement.classList.contains('note__text')){
+        // only toggle the selected class if the element is the parent li or the
+        // the icon within it
+        if(toolElement.tagName === 'BUTTON'){        
+            toolElement.classList.toggle('toolbar__tool--is-selected');
+            toggleToolById(toolElement.id);   
+        }   
+
+        if(toolElement.tagName === 'I'){
+            toolElement.parentElement.classList.toggle('toolbar__tool--is-selected')
+            toggleToolById(toolElement.parentElement.id);  
+        }
+    }     
 
 }, false);
 
@@ -57,6 +60,10 @@ toolbar.addEventListener('mousedown', (evt) => {
 function toggleToolById(toolId){        
     for(let i = 0; i < editingTools.length; i++){
         if(editingTools[i].toolName === toolId){
+            // if(toolId === 'insertUnorderedList' || toolId === 'insertOrderedList'){
+            //     document.execCommand("indent");
+            //     console.log("matched" + toolId);
+            // }            
             editingTools[i].isToggled = !editingTools[i].isToggled;
             document.execCommand(toolId);
         }
@@ -65,20 +72,8 @@ function toggleToolById(toolId){
 
 
 
-// get reference to note elements, maybe use for copy paste?
+// get reference to note element parent for event delegation
 const noteGrid = document.querySelector('.js-note-grid');
-noteGrid.addEventListener('select', (evt) => {
-
-    // if(document.activeElement.nodeName.toLowerCase() == 'div'){
-    //     const textarea = document.activeElement;
-    //     let selectedText = getSelectedText(textarea);
-    // }
-}, false);
-
-// function getSelectedText(textInputElement){
-//     return textInputElement.value.substring(textInputElement.selectionStart, textInputElement.selectionEnd)
-// }
-
 
 // Add note functionality
 const addButton = document.querySelector('#addNote');
@@ -123,27 +118,93 @@ noteGrid.addEventListener('mousedown', (evt)=> {
             evt.target.closest('.note').remove();
             // alert('Note deleted');
         }
+    }  
+    
+    // hide menu if the grid is clicked
+    if(menu.classList.contains('menu--is-visible')){
+        menu.classList.toggle('menu--is-visible');
+        hamburgerIcon.classList.toggle('hamburger--minus');
     }
 }, false);
 
-// [x] TODO: add note button styling    
-// [x] TODO: add note button behavior
-// [x] TODO: remove note button styling
-// [x] TODO: remove note button behavior
-// TODO: ul button behavior bug
-// TODO: ol button behavior bug
+// deactivate toolbar when notes are not selected
+noteGrid.addEventListener('blur', (evt)=>{
+    if(evt.target.classList.contains('note__text')){
+        // deselect all tools
+        editingTools.forEach(tool => {
+            tool.isToggled = false;
+        });
+        for(let i = 0; i < toolbar.children.length; i++){
+            toolbar.children[i].classList.remove('toolbar__tool--is-selected');
+        }
+    }
+}, true);
+
+
+const searchBar = document.querySelector('.js-searchbar');
+searchBar.addEventListener('keyup', (evt)=>{
+    // revert previous split text and span insert
+    for(let i = 0; i < noteGrid.children.length; i ++){
+        noteGrid.children[i].firstElementChild.innerHTML = noteGrid.children[i].firstElementChild.innerText;
+        console.log(noteGrid.children[i].firstElementChild.textContent);
+    }
+
+
+    if(evt.target.value){
+        // console.log('value of search: ' + evt.target.value);
+        searchNotes(evt.target.value);
+    }
+}, false);
+
+function searchNotes(searchText){
+    // for each note in the grid
+    for(let i = 0; i < noteGrid.children.length; i ++){
+        if(noteGrid.children[i].firstElementChild.innerText.indexOf(searchText) >= 0){
+            let fullNoteString = noteGrid.children[i].firstElementChild.innerText;
+            let searchTextStart = fullNoteString.indexOf(searchText);
+            let searchTextEnd = searchTextStart + searchText.length;
+            
+            // console.log(noteGrid.children[i].firstElementChild.firstChild); // text node
+            const fullStringTextNode = noteGrid.children[i].firstElementChild.firstChild;
+            // // split 3/3 string
+            const thirdSubstr = fullStringTextNode.splitText(searchTextEnd).textContent;
+            // // split 2/3 string (searchtext)
+            const secondSubstr = fullStringTextNode.splitText(searchTextStart).textContent;
+            // // split 1/3 string
+            const firstSubstr = fullStringTextNode.textContent;
+
+            // console.log(`first: ${firstSubstr}\nsecond: ${secondSubstr}\nthird: ${thirdSubstr}`);
+            
+            // rejoin as innerHTML and embed 2/3 into a span with styling
+            noteGrid.children[i].firstElementChild.innerHTML = firstSubstr + "<span class=\"text--highlight\">" + secondSubstr + "</span>" + thirdSubstr;
+
+            //TODO: insert this function in a while(searchtext is found in string) instead of if
+            // j++ to get all instances of the word in a note
+        }
+    }
+}
+
+
+
+// [x] add note button styling    
+// [x] add note button behavior
+// [x] remove note button styling
+// [x] remove note button behavior
 // TODO: code button pressed event
-// TODO: search for text implementation
+// TODO: search for text implementation -- add multiple search terms found
+// TODO: change edit tools to buttons and disable when not focused on a note
 // TODO: authentication
 // TODO: cloud database connection
 // TODO: Session - State management
 // TODO: JS modularity
-// TODO: HTML Semantics
+// TODO: HTML Semantics and Accesibility Check
 
 
 // BUGS: 
 // Edit buttons dont affect CTRL+A text
 // Clicking delete button removes not focus.
+// ul button behavior bug
+// ol button behavior bug
 
 // EXTRAS:
 // Note deleted custom alert
