23dd455662c13c18a14be72ea62485492d20df5d
Edgar Mancia <eddiemm@users.noreply.github.com>
2021-09-17 15:05:15 -0700

    Get user values from login forms

--- js/login.js
+++ js/login.js
@@ -13,3 +13,45 @@ signInLink.addEventListener('click', (evt)=>{
     signUpForm.classList.add('hide');
     signInForm.classList.remove('hide');
 }, false);
+
+
+// get the user credentials - username and password
+let username, userPassword;
+const usernameInput = signInForm.children[1];
+const passwordInput = signInForm.children[2];
+
+usernameInput.addEventListener('keyup', function (evt){
+    username = this.value;
+    console.log(username);
+}, false);
+
+passwordInput.addEventListener('keyup', function (evt){
+    userPassword = this.value;
+    console.log(userPassword);
+}, false);
+
+let newUserEmail, newUserName, newUserPassword, newUserPasswordConfirmed;
+const newUserEmailInput = signUpForm.children[1];
+const newUserNameInput = signUpForm.children[2];
+const newUserPasswordInput = signUpForm.children[3];
+const newUserPasswordConfirmedInput = signUpForm.children[4];
+
+newUserEmailInput.addEventListener('keyup', function (evt){
+    newUserEmail = this.value;
+    console.log(newUserEmail);
+}, false);
+
+newUserNameInput.addEventListener('keyup', function (evt){
+    newUserName = this.value;
+    console.log(newUserName);
+}, false);
+
+newUserPasswordInput.addEventListener('keyup', function (evt){
+    newUserPassword = this.value;
+    console.log(newUserPassword);
+}, false);
+
+newUserPasswordConfirmedInput.addEventListener('keyup', function (evt){
+    newUserPasswordConfirmed = this.value;
+    console.log(newUserPasswordConfirmed);
+}, false);
